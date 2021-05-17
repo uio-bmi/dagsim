fc4b4db8eff1fe63e3b2de305cbed7c23ec75471
ghadialhajj <ghadi.alhajj8@gmail.com>
2021-05-17 17:08:30 +0200

    Added docstrings and a main_run to processPlates.py

--- utils/processPlates.py
+++ utils/processPlates.py
@@ -1,22 +1,14 @@
-p1 = ['A']
-p2 = ['A', 'B']
-p3 = ['C']
-p4 = ['A', 'B', 'D', 'E', 'F', 'G', 'H', 'I']
-p5 = ['D', 'E']
-p6 = ['B']
-p7 = ['G', 'H', 'I']
-
-listPlates = [p1, p2, p3, p4, p5, p6, p7]
-listLabels = ["p1", "p2", "p3", "p4", "p5", "p6", "p7"]
-subs_dict = {i: "subgraph cluster_" + str(i) + " {" + ", ".join(listPlates[i - 1]) + ";\n label=" + listLabels[i - 1]
-             for i in
-             range(1, len(listPlates) + 1)}
-
-
-# elem = ['A', 'B', 'D', 'E', 'F']
-# large_list = [['A'], ['A', 'B'], ['C'], ['B'], ['E', 'D']]
-
 def find_order(list_of_plates: list):
+    """ A function that finds which plates are sub-plates of other plates.
+
+    Args:
+        list_of_plates (list): A list of lists, where each sublist contains the nodes found in the plate with
+            the corresponding index.
+
+    Returns:
+        dict: A dictionary of the sub-plates, if any, of each plate.
+
+    """
     resDict = {k: [] for k in range(1, len(list_of_plates) + 1)}
     for plate in list_of_plates:
         if len(plate) != 1:
@@ -28,6 +20,15 @@ def find_order(list_of_plates: list):
 
 
 def process_dict(redun_dict: dict):
+    """ A function that finds the direct child plates, if any, of each plate.
+
+    Args:
+        redun_dict (dict): A dictionary containing the, possibly redundant, sub-plates of each plate.
+
+    Returns:
+        dict: A dictionary of the direct child plates, if any, of each plate.
+
+    """
     innder_plateIdxs = set([item for sublist in redun_dict.values() for item in sublist])
     for plate in innder_plateIdxs:
         keysPlate = [key for key in redun_dict.keys() if plate in redun_dict[key]]
@@ -47,80 +48,79 @@ def process_dict(redun_dict: dict):
     return redun_dict
 
 
-def transform(my_dict, root_plates, subs_dict):
+def transform(processed_dict, root_plates, subs_dict):
+    """ A function that generates the .dot string for all the plates in the graph, in the correct order.
+
+    Args:
+        processed_dict (dict): A dictionary of the direct child plates, if any, of each plate.
+        root_plates (list): A list of the root plates.
+        subs_dict (dict): A dictionary of strings to define each plate in .dot format.
+
+    Returns:
+        string: A string defining all the plates and their nodes, in .dot format.
+
+    """
     empL = ""
     lvl = 0
 
-    def prt2(my_dict, root_plates):
+    def plate_string(processed_dict, root_plates):
         nonlocal empL, lvl, subs_dict
         for idx, root in enumerate(root_plates):
             empL = empL + subs_dict[root] + "\n"
-            if not my_dict[root]:
+            if not processed_dict[root]:
                 if idx == (len(root_plates) - 1):
                     lvl -= 1
                     empL = empL + "}"
             else:
                 lvl += 1
-                prt2(my_dict, my_dict[root])
+                plate_string(processed_dict, processed_dict[root])
                 empL = empL[:-1]
             empL = empL + "}"
 
-    prt2(my_dict, root_plates)
+    plate_string(processed_dict, root_plates)
     return empL
 
 
-def find_roots(my_dict):
-    root_plates = list(range(1, len(my_dict.keys()) + 1))
-    # print(root_plates)
-    for key in my_dict.keys():
-        for val in my_dict[key]:
+def find_roots(processed_dict):
+    """ A function that finds the plates that are not contained in other plates.
+
+    Args:
+        processed_dict (dict): A dictionary of the direct child plates, if any, of each plate.
+
+    Returns:
+        list: A list of the root plates.
+
+    """
+    root_plates = list(range(1, len(processed_dict.keys()) + 1))
+    for key in processed_dict.keys():
+        for val in processed_dict[key]:
             root_plates.pop(root_plates.index(val))
     return root_plates
 
 
-# my_dict = find_order(listPlates)
-# print(my_dict)
-#
-# my_dict = process_dict(my_dict)
-# print(my_dict)
-#
-# root_plates = find_roots(my_dict)
-#
-# empL = transform(my_dict, root_plates, subs_dict)
-#
-# print(empL)
-
-
 def get_plate_dot(plate_embedding):
+    """ A wrapper function that generates the part of the .dot string corresponding to the plates.
+
+    Args:
+        plate_embedding (dict): A dictionary containing the label and nodes of each plate.
+
+    Returns:
+        str: A string defining the plates in .dot format.
+
+    """
     subs_dict = {
-        i: "subgraph cluster_" + str(i) + " {" + ", ".join(plate_embedding[i][1]) + ';\n label="' + plate_embedding[i][0] + '"'
-        for i in range(1, len(plate_embedding.keys()))}
+        i: "subgraph cluster_" + str(i) + " {" + ", ".join(plate_embedding[i][1]) + ';\n label="' +
+           plate_embedding[i][0] + '"' for i in range(1, len(plate_embedding.keys()))}
     ordered_dict = find_order([plate_embedding[i][1] for i in range(1, len(plate_embedding.keys()))])
     processed_dict = process_dict(ordered_dict)
     root_plates = find_roots(processed_dict)
     dot_string = transform(processed_dict, root_plates, subs_dict)
     return dot_string
-# # default transform
-# def transform(my_dict, root_plates):
-#     empL = ""
-#     lvl = 0
-#
-#     def prt2(my_dict, root_plates):
-#         nonlocal empL, lvl
-#         for idx, root in enumerate(root_plates):
-#             empL = empL + str(root) + "{"  # subs_dict[root] + "\n"
-#             if not my_dict[root]:
-#                 if lvl != 0:
-#                     if idx == (len(root_plates) - 1):
-#                         lvl -= 1
-#                         empL = empL + "}"
-#             else:
-#                 # empL = empL + "{"
-#                 lvl += 1
-#                 prt2(my_dict, my_dict[root])
-#                 empL = empL[:-1]
-#             empL = empL + "}"
-#
-#     prt2(my_dict, root_plates)
-#     empL = empL[:-1]
-#     return empL
+
+
+if __name__ == "__main__":
+    # Sample plate_embedding
+    plate_embedding = {0: (None, ['Prior', 'Node4']), 1: ('plate_1', ['Node1', 'Node2']),
+                       2: ('plate_2', ['Node5', 'Node1', 'Node2'])}
+    # Note that the 0th plate is excluded from the .dot string generation.
+    print(get_plate_dot(plate_embedding))
