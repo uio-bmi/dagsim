f5519cdb43b2f4a1ebc37ef7862552fb85c587ac
ghadialhajj <ghadi.alhajj8@gmail.com>
2021-06-20 18:44:47 +0200

    Add preliminary YAML3 with code

---
+++ sample_specs/YAML3/yaml3.py
@@ -0,0 +1,20 @@
+from baseDS import Graph, Generic, Selection
+from scipy.stats import norm, bernoulli
+
+
+def isEight(inp):
+    return inp == 8
+
+# def identity():
+#     return 0.5
+
+
+Prior = Generic(name="Prior1", function=norm.rvs, additional_params=[0.5, 0.1])
+Node = Generic(name="Node1", function=bernoulli.rvs, parents=[Prior], additional_params=[7])
+Selection = Selection(name="Selection", function=isEight, parents=[Node])
+
+listNodes = [Prior, Node, Selection]
+my_graph = Graph("Graph1", listNodes)
+my_graph.draw()
+n = my_graph.simulate(num_samples=20, csv_name="test")
+print(n)
