d1a6e84869b6e5c2f58d65e2bf542edeaf751535
ghadialhajj <ghadi.alhajj8@gmail.com>
2021-06-20 19:16:38 +0200

    Correct YAML2 using a wrapper.

--- sample_specs/YAML2/yaml2.py
+++ sample_specs/YAML2/yaml2.py
@@ -1,20 +1,24 @@
 from baseDS import Graph, Generic, Selection
-from scipy.stats import norm, bernoulli
+from scipy.stats import norm, binom
 
 
-def isEight(inp):
+def is_eight(inp):
     return inp == 8
+
 
 # def identity():
 #     return 0.5
 
+def binom_wrapper(prob, trials):
+    return binom.rvs(n=trials, p=prob)
+
 
 Prior = Generic(name="Prior1", function=norm.rvs, additional_params=[0.5, 0.1])
-Node = Generic(name="Node1", function=bernoulli.rvs, parents=[Prior], additional_params=[7])
-Selection = Selection(name="Selection", function=isEight, parents=[Node])
+Node = Generic(name="Node1", function=binom_wrapper, parents=[Prior], additional_params=[10])
+Selection = Selection(name="Selection", function=is_eight, parents=[Node])
 
 listNodes = [Prior, Node, Selection]
 my_graph = Graph("Graph1", listNodes)
 my_graph.draw()
-n = my_graph.simulate(num_samples=20, csv_name="test")
+n = my_graph.simulate(num_samples=50, csv_name="test")
 print(n)
