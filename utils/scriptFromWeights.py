86d76de3ac070c8e2cdbdd7209fb405d8e718f50
ghadialhajj <ghadi.alhajj8@gmail.com>
2021-08-27 23:13:48 +0200

    Add a file to create a python script from a weight matrix and SEM types.

---
+++ utils/scriptFromWeights.py
@@ -0,0 +1,83 @@
+from baseDS import Graph, Selection, Stratify, Generic
+import numpy as np
+import igraph as ig
+
+
+def import_notears(weight_matrix: np.ndarray, top_order: list, sem_type: str = ""):
+
+    def create_function(name, weight_vector, sem_type):
+        non_zero_indices = [i for i, e in enumerate(weight_vector) if e != 0]
+        parents_names = ", ".join(["x" + str(i) for i in non_zero_indices])
+        linear_equation = " + ".join(
+                [str(weight_vector[e]) + " * x" + str(non_zero_indices[i]) for i, e in enumerate(non_zero_indices)])
+        func_str = "def func_" + name + "(" + parents_names + "):\n\t" + name + " = "
+        if sem_type not in ["logistic", "poisson"]:
+            func_str += linear_equation + " + "
+            if sem_type == "gauss":
+                func_str += "np.random.normal(loc=1, scale=0)"
+            elif sem_type == 'exp':
+                func_str += "np.random.exponential(loc=1, scale=0)"
+            elif sem_type == 'gumbel':
+                func_str += "np.random.gumbel(loc=1, scale=0)"
+            elif sem_type == 'uniform':
+                func_str += "np.random.uniform(low=-1, high=1)"
+        elif sem_type == 'logistic':
+            func_str += "np.random.binomial(1, sigmoid(" + linear_equation + ")) * 1.0"
+        elif sem_type == 'poisson':
+            func_str += "np.random.poisson(1, np.exp(" + linear_equation + ")) * 1.0"
+        else:
+            raise ValueError('unknown sem type')
+        func_str += "\n\treturn " + name
+
+        return func_str
+
+    def create_node(name: str, parents: list = None):
+        # args_str = {"x" + str(i): "Node_x" + str(i) for i in parents}
+        node_def = "Node_" + name + " = Generic(name='" + name + "', function="
+        if parents:
+            arg_string = "{"
+            for i in parents:
+                arg_string += "'x" + str(i) + "': Node_" + "x" + str(i) + ", "
+            arg_string = arg_string[:-2] + "}"
+            node_def += "func_" + name + ", arguments=" + arg_string + ")"
+        else:
+            node_def += "np.random.normal, arguments={'loc': 1, 'scale': 0})"
+        return node_def
+
+    imports = "from baseDS import Graph, Selection, Stratify, Generic \n" \
+              "import numpy as np \n\n\n"
+
+    functions = ""
+    for index, column in enumerate(weight_matrix.T):
+        if any(column):
+            functions += create_function("x" + str(index), column, sem_type)
+            functions += "\n\n\n"
+
+    nodes = ""
+    for node_index in top_order:
+        if any(weight_matrix.T[node_index]):
+            non_zero_indices = [i for i, e in enumerate(weight_matrix.T[node_index]) if e != 0]
+            nodes += create_node("x" + str(node_index), non_zero_indices)
+            nodes += "\n"
+        else:
+            nodes += create_node("x" + str(node_index))
+            nodes += "\n"
+
+    graph_def = "\nlistNodes = ["
+    for node_index in top_order:
+        graph_def += "Node_x" + str(node_index) + ", "
+    graph_def = graph_def[:-2] + "]\n"
+    graph_def += "graph = Graph('Graph1', listNodes) \n"
+
+    return imports + functions + nodes + graph_def
+
+
+if __name__ == "__main__":
+    weight = np.array([[0, 0, 2, 1], [0, 0, 3, 0], [0, 0, 0, 1], [0, 0, 0, 0]])
+
+    G = ig.Graph.Weighted_Adjacency(weight.tolist())
+    ordered_vertices = G.topological_sorting()
+
+    script = import_notears(weight, ordered_vertices, sem_type="gauss")
+    with open("test.py", "w") as file:
+        file.write(script)
