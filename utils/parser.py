ad5aed0183b0a0554bdee5dc753878040f08b39d
ghadialhajj <ghadi.alhajj8@gmail.com>
2021-09-27 17:08:20 +0200

    divided into subfunctions and made it more efficient

--- utils/parser.py
+++ utils/parser.py
@@ -11,28 +11,24 @@ import igraph as ig
 
 
 def parse_yaml(file_name: str):
-    def build_adj_matrix(nodes_yaml: dict):
+    def build_adj_matrix(nodes: dict):
         # TODO add a note to make sure that no string unintentionally has the same name as a node
-        node_names = nodes_yaml.keys()
-        parents_dict = {k: [v for v in nodes_yaml[k]["arguments"].values() if v in node_names] for k in node_names}
+        node_names = nodes.keys()
+        parents_dict = {k: [v for v in nodes[k]["arguments"].values() if v in node_names] for k in node_names}
         pd_dict = pd.DataFrame(0, columns=node_names, index=node_names)
         for child in node_names:
             for parent in parents_dict[child]:
                 pd_dict[child][parent] = 1
         return pd_dict.to_numpy(), list(node_names)
 
-    def get_top_order():
-        pdDAG, names = build_adj_matrix(yaml_file["graph"]["nodes"])
-        print(names)
+    def get_top_order(pdDAG, names):
         G = ig.Graph.Weighted_Adjacency(pdDAG.tolist())
         top_order = G.topological_sorting()
         top_order = [names[i] for i in top_order]
         return top_order
 
-    def check_acyclicity():
-        pdDAG, _ = build_adj_matrix(yaml_file["graph"]["nodes"])
-        # print(ppd.to_numpy())
-        pdDAGGraph = ig.Graph.Weighted_Adjacency(pdDAG.tolist())
+    def check_acyclicity(adj_mat):
+        pdDAGGraph = ig.Graph.Weighted_Adjacency(adj_mat.tolist())
         return ig.Graph.is_dag(pdDAGGraph)
 
     def get_func_by_name(functions_list: list, func_name: str):
@@ -69,12 +65,71 @@ def parse_yaml(file_name: str):
         #     raise ModuleNotFoundError("no lib")
         #     return None
 
+    def split_func_and_args(func_expression: str):
+        func_expression = func_expression.replace(" ", "")
+        args_str = func_expression[func_expression.find("(") + 1: func_expression.find(")")]
+        args_str = args_str.split(",")
+        args_dict = {}
+        for arg in args_str:
+            arg_name = arg[:arg.find("=")]
+            args_dict[arg_name] = arg[arg.find("=")+1:]
+            try:
+                args_dict[arg_name] = float(args_dict[arg_name])
+            except ValueError:
+                pass
+        func_name = func_expression[:func_expression.find("(")]
+        return func_name, args_dict
+
+    def parse_string_args(nodes):
+        for key in nodes.keys():
+            if "(" in nodes[key]["function"]:
+                print(key)
+                nodes[key]["function"], nodes[key]["arguments"] = split_func_and_args(
+                    nodes[key]["function"])
+        return nodes
+
+    def populate_graph_from_nodes(nodes: dict, functions: list):
+        nonlocal my_graph
+        for key in top_order:
+
+            nodes[key] = {**nodes[key], **{"name": key}}
+            nodes[key]["function"] = get_func_by_name(functions, nodes[key]["function"])
+            nodes[key]["arguments"] = {
+                k: my_graph.get_node_by_name(v) if my_graph.get_node_by_name(v) is not None else v for k, v in
+                nodes[key]["arguments"].items()}
+
+            node_type = nodes[key].get("type")
+            if node_type is not None:
+                nodes[key].pop("type")
+
+            if node_type == "Generic" or node_type is None:
+                node = Generic.build_object(**nodes[key])
+                my_graph.add_node(node)
+
+            elif node_type == "Selection":
+                node = Selection.build_object(**nodes[key])
+                my_graph.add_node(node)
+
+            elif node_type == "Stratify":
+                node = Stratify.build_object(**nodes[key])
+                my_graph.add_node(node)
+
+            else:
+                raise TypeError("\"" + node_type + "\" is not a valid node type. \"type\" should be either Generic, "
+                                                   "Selection, or Stratify.")
+        return my_graph
+
     with open(file_name, 'r') as stream:
         yaml_file = yaml.safe_load(stream)
 
-    assert check_acyclicity(), "The graph is not acyclic."
     nodes_dict = yaml_file["graph"]["nodes"]
-    top_order = get_top_order()
+    nodes_dict = parse_string_args(nodes_dict)
+
+    adj_matrix, node_names = build_adj_matrix(nodes_dict)
+
+    assert check_acyclicity(adj_matrix), "The graph is not acyclic."
+
+    top_order = get_top_order(adj_matrix, node_names)
     print(top_order)
 
     functions_file = importlib.import_module(yaml_file["graph"]["python_file"])
@@ -83,34 +138,7 @@ def parse_yaml(file_name: str):
     # print(functions_list)
 
     my_graph = Graph(yaml_file["graph"]["name"], [])
-
-    for key in top_order:
-
-        nodes_dict[key] = {**nodes_dict[key], **{"name": key}}
-        nodes_dict[key]["function"] = get_func_by_name(functions_list, nodes_dict[key]["function"])
-        nodes_dict[key]["arguments"] = {
-            k: my_graph.get_node_by_name(v) if my_graph.get_node_by_name(v) is not None else v for k, v in
-            nodes_dict[key]["arguments"].items()}
-
-        node_type = nodes_dict[key].get("type")
-        if node_type is not None:
-            nodes_dict[key].pop("type")
-
-        if node_type == "Generic" or node_type is None:
-            node = Generic.build_object(**nodes_dict[key])
-            my_graph.add_node(node)
-
-        elif node_type == "Selection":
-            node = Selection.build_object(**nodes_dict[key])
-            my_graph.add_node(node)
-
-        elif node_type == "Stratify":
-            node = Stratify.build_object(**nodes_dict[key])
-            my_graph.add_node(node)
-
-        else:
-            raise TypeError("\"" + node_type + "\" is not a valid node type. \"type\" should be either Generic, "
-                                               "Selection, or Stratify.")
+    my_graph = populate_graph_from_nodes(nodes_dict, functions_list)
 
     print(my_graph)
     data = my_graph.simulate(**yaml_file["instructions"]["simulation"])
