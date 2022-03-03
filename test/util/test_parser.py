043fc53a416fb2c76269def10404b91b552a6668
ghadialhajj <ghadi.alhajj8@gmail.com>
2022-03-03 13:19:58 +0100

    Fix parser test + importing from_matrix

--- test/util/test_parser.py
+++ test/util/test_parser.py
@@ -7,12 +7,12 @@ import numpy as np
 class TestParser(unittest.TestCase):
 
     def test_working_file(self):
-        np.random.seed(0)
+        np.random.seed(1)
         parser = DagSimSpec(file_name="test_yaml.yml")
         data = parser.parse(draw=False, verbose=False)
         print(data)
-        self.assertEqual(['aaaaaa', ''], data["result"])
-        np.testing.assert_almost_equal([1.7640, 0.4001], data["source"], decimal=4)
+        self.assertEqual(['aaa', 'aaaa'], data["result"])
+        np.testing.assert_equal([3, 4], data["source"])
 
 
 if __name__ == '__main__':
