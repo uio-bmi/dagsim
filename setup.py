58f40857e7caf62538c7d1b4d5fe18c28ef33fc1
ghadialhajj <ghadi.alhajj8@gmail.com>
2023-02-01 10:09:06 +0100

    Increment version to 1.0.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dagsim',
-    version='1.0.5',
+    version='1.0.8',
     packages=find_packages(),
     url='https://github.com/uio-bmi/dagsim',
     license='AGPL-3.0 License',
