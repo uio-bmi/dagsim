05886387a6249599c1b5ab5a1227a9031d6a8775
Milena Pavlovic <milenapavlovic775@gmail.com>
2025-05-02 09:31:31 +0200

    Update version to 1.0.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dagsim',
-    version='1.0.9',
+    version='1.0.10',
     packages=find_packages(),
     url='https://github.com/uio-bmi/dagsim',
     license='AGPL-3.0 License',
